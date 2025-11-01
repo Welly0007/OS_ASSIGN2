1d9293021f00175356f8f141ad8f1a60dbc8c25d
MohammedHany123 <midohany1807@gmail.com>
2025-11-01 21:31:45 +0200

    Added semaphore

---
+++ src/Semaphore.java
@@ -0,0 +1,29 @@
+public class Semaphore {
+    protected int value = 0;
+    
+    protected Semaphore(){
+        value = 0;
+    }
+
+    protected Semaphore(int initial){
+        value = initial;
+    }
+
+    public synchronized void P() {
+        value--;
+        if (value < 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public synchronized void V() {
+        value++;
+        if (value <= 0) {
+            notify();
+        }
+    }
+}
